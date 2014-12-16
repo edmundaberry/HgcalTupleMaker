8fffaa2a8c0b16378df1d532160ad8959f29a76a
Edmund <edmund.a.berry@cern.ch>
2014-12-16 04:15:19 +0100

    Blank

--- python/HgcalTupleMaker/HgcalTupleMaker_cfi.py
+++ python/HgcalTupleMaker/HgcalTupleMaker_cfi.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_Event_cfi        import * 
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_GenParticles_cfi import * 
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_GenJets_cfi      import * 
-from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_PFJets_cfi       import * 
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_PFPrunedJets_cfi import * 
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_PFCands_cfi      import * 
 from HGCALANA.HgcalTupleMaker.HgcalTupleMaker.HgcalTupleMaker_PFClusters_cfi   import * 
