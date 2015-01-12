184ade2a0eb7a81def68efcaa34c156e10dd1c3b
Edmund <edmund.a.berry@cern.ch>
2015-01-12 14:09:55 +0100

    Added rho for L1 jet correcctions

--- python/HgcalTupleMaker/HgcalTupleMaker_Event_cfi.py
+++ python/HgcalTupleMaker/HgcalTupleMaker_Event_cfi.py
@@ -1,3 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-hgcalTupleEvent = cms.EDProducer("HgcalTupleMaker_Event")
+hgcalTupleEvent = cms.EDProducer("HgcalTupleMaker_Event",
+   RhoInputTag = cms.InputTag("kt6PFJets", "rho")
+)
