17da9b513582f476d88719bfcc2e581ca5b5909e
Edmund <edmund.a.berry@cern.ch>
2015-03-22 16:46:12 +0100

    Added new jet cone sizes

--- python/HgcalTupleMaker/HgcalTupleMaker_PFPrunedJets_cfi.py
+++ python/HgcalTupleMaker/HgcalTupleMaker_PFPrunedJets_cfi.py
@@ -9,7 +9,33 @@ ca4PFJetsCHSPrunedRCut5.jetCollInstanceName = cms.string("SubJets")
 ca4PFJetsCHSPrunedRCut5.writeCompound = cms.bool(True)
 ca4PFJetsCHSPrunedRCut5.jetPtMin = cms.double(1.)
 
+ak0p5PFJetsCHS = ak5PFJetsCHS.clone( rParam = 0.05 )
+ak1p0PFJetsCHS = ak5PFJetsCHS.clone( rParam = 0.10 )
 ak1p5PFJetsCHS = ak5PFJetsCHS.clone( rParam = 0.15 )
+
+hgcalTuplePFAK0p5Jets = cms.EDProducer("HgcalTupleMaker_PFJets",
+    RawJetInputTag      = cms.InputTag("ak0p5PFJetsCHS"),
+    TrimmedJetInputTags = cms.VInputTag(),
+    SubjetInputTags     = cms.VInputTag(),
+    PFCandInputTag      = cms.InputTag("selectPFCands"),
+    RCutFactorNames     = cms.vstring (),
+    RCutFactors         = cms.vdouble (),
+    Prefix = cms.string('PFAK0p5Jet'),
+    Suffix = cms.string(''),
+    MaxSize = cms.uint32(10)
+)
+
+hgcalTuplePFAK1p0Jets = cms.EDProducer("HgcalTupleMaker_PFJets",
+    RawJetInputTag      = cms.InputTag("ak1p0PFJetsCHS"),
+    TrimmedJetInputTags = cms.VInputTag(),
+    SubjetInputTags     = cms.VInputTag(),
+    PFCandInputTag      = cms.InputTag("selectPFCands"),
+    RCutFactorNames     = cms.vstring (),
+    RCutFactors         = cms.vdouble (),
+    Prefix = cms.string('PFAK1p0Jet'),
+    Suffix = cms.string(''),
+    MaxSize = cms.uint32(10)
+)
 
 hgcalTuplePFAK1p5Jets = cms.EDProducer("HgcalTupleMaker_PFJets",
     RawJetInputTag      = cms.InputTag("ak1p5PFJetsCHS"),
@@ -36,7 +62,7 @@ hgcalTuplePFCA4PrunedJets = cms.EDProducer("HgcalTupleMaker_PFJets",
     MaxSize = cms.uint32(10)
 )
 
-myJetProducerSequence         = cms.Sequence ( ca4PFJetsCHS * ak1p5PFJetsCHS )
+myJetProducerSequence         = cms.Sequence ( ca4PFJetsCHS * ak0p5PFJetsCHS * ak1p0PFJetsCHS * ak1p5PFJetsCHS )
 myPrunedJetProducerSequence   = cms.Sequence ( ca4PFJetsCHSPrunedRCut5 ) 
 myPrunedJetTupleMakerSequence = cms.Sequence ( hgcalTuplePFCA4PrunedJets ) 
                                
